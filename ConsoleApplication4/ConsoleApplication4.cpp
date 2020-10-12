89dbe4370dcdc477cf8aabaf59264156cb3a8044
lparadylo <lukaszparadylo@gmail.com>
2020-10-12 17:52:11 +0200

    zmiana koncepcji

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,25 +1,35 @@
-﻿#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+﻿#include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
-int main(int argc, char** argv)
+int main()
 {
-    if (argc != 2)
+    /*Mat rgb, gry, cny;
+
+    rgb = imread("C:/Users/lparadylo/source/repos/ConsoleApplication3/x64/Debug/FLIR0838.JPG");
+    cvtColor(rgb, gry, COLOR_BGR2GRAY);
+    imshow("EX1", gry);
+    Canny(gry, cny, 10, 100, 3, true);
+    imshow("EX2", cny);
+    cv::waitKey(0);*/
+
+    VideoCapture cap;
+    cap.open(0);
+    Mat frame;
+    Mat frame2;
+    Mat frame3;
+    for (;;)
     {
-        cout << " Usage: " << argv[0] << " ImageToLoadAndDisplay" << endl;
-        return -1;
+        cap >> frame;
+        cvtColor(frame, frame2, COLOR_BGR2GRAY);
+        Canny(frame, frame3, 10, 100, 3, true);
+        if (!cap.isOpened()) break;
+        imshow("ex", frame2);
+        imshow("ex1", frame3);
+        if (cv::waitKey(33) >= 0) break;
     }
-    Mat image;
-    image = imread(argv[1], IMREAD_COLOR); // Read the file
-    if (image.empty()) // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl;
-        return -1;
-    }
-    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", image); // Show our image inside it.
-    waitKey(0); // Wait for a keystroke in the window
+    
+
+
     return 0;
 }
